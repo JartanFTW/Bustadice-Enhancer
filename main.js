b345e872a06e54a7ce28c4bf44487d36f4e981a3
Jartan <66337180+JartanFTW@users.noreply.github.com>
2020-09-21 02:32:54 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+class enhancer {
+	static #instances = 0;
+
+	constructor (app) {
+		enhancer.#instances++;
+		if (enhancer.#instances > 1) {
+			throw new Error("Unable to create more than one enhancer instance");
+		}
+		
+		this.#app = app
+		this.profit = 0;
+		this.rolls = 0;
+		this.seedRolls = 0;
+	}
+	
+	#roundBet (wager) {
+		return Math.max( 100, Math.round ( wager / 100 ) * 100 )
+	}
+	
+	bet (wager, target) {
+		// Insert checkLoss/checkProfit here.
+		var roll = await this.#app.bet(#roundBet(wager), target)
+		this.rolls++
+		this.seedRolls++
+		if (roll.multiplier < target) { profit -= roundBet(bet) } else { profit += roundBet(bet) * target - roundBet(bet) }
+		return roll
+		
+	}
+}
